da191fe602ee1450be9d3b76b18cf8a940068e20
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-10-17 19:12:17 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sashiel_ST10028058_PROG6212_Part2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
