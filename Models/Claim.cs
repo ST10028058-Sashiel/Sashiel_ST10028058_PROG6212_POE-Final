99b66bc97977e5df50be1797141823b704316324
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2024-10-17 19:55:08 +0200

    Added Claim Model for all Requirements like the Lecturer Name, HoursWorked, HourlyRate, Status, SupportingDocument.

---
+++ Models/Claim.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sashiel_ST10028058_PROG6212_Part2.Models
+{
+    public class Claim
+    {
+        [Key]
+        public int ClaimId { get; set; }
+
+        [Required]
+        [Display(Name = "Lecturer")]
+        public string LecturerName { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double HoursWorked { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double HourlyRate { get; set; }
+
+        [Display(Name = "Additional Notes")]
+        public string Notes { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Pending";
+
+        [Required]
+        public string? SupportingDocumentPath { get; set; }
+    }
+}
